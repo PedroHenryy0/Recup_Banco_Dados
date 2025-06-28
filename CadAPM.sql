6665251fc6687fa5541aab7e5748bbff5531a5cf
PedroHenryy0 <pedro_h_sm_@outlook.com>
2025-06-27 21:14:52 -0300

    CADAPM - Preenchimento
    Preenchimento das informações de alunos, professor e matéria

---
+++ CadAPM.sql
@@ -0,0 +1,29 @@
+
+INSERT INTO alunos (alunoname, idade)
+VALUES
+('Pedro', '19'),
+('Matheus', '33'),
+('Lucas', '30'), -- 3
+('Elias', '30'),
+('Enoque', '30'),
+('João', '30'), -- 6
+('Xandão', '27'),
+('Rodrigo', '30'),
+('Weslaine', '21'); -- 9
+
+INSERT INTO disciplinas (nomemateria)
+VALUES
+('Matemática'),
+('História'),
+('Música');
+
+INSERT INTO alunoFS (alunonome, idade)
+VALUES
+('Freeddy Fazbear', '9');
+
+INSERT INTO professor (profenome)
+VALUES
+('Ivens Diego');
+
+
+
